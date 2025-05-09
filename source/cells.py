01f2a117f71c221970bb9f083e34a08cfe3291cb
kingilsildor <tychostam@gmail.com>
2025-05-09 17:28:28 +0200

    add cell template

---
+++ source/cells.py
@@ -0,0 +1,39 @@
+import numpy as np
+from config import *
+
+class Cell():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.alive = True
+        self.age = 0 # in timesteps
+
+    def secrete(self):
+        if not self.alive:
+            return 0
+        ...
+
+    def die(self):
+        if not self.alive:
+            return 0
+        
+        self.alive = False
+
+    def age_cell(self) -> int:
+        if not self.alive:
+            return 0
+        
+        self.age += 1
+        P_death = 1 - np.exp(-(BASE_DEATH_RATE * self.age)**SHAPE_PARAM)
+
+        if np.random.random() >= P_death:
+            self.die()
+            return 2
+        
+        return 1
+
+    def get_coordinates(self):
+        return self.x, self.y
+    
+    def get_age(self):
+        return self.age
