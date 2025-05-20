d5d89aabb34ebc10989a596854176d311c6c3d65
kingilsildor <tychostam@gmail.com>
2025-05-20 15:49:17 +0200

    update GIF

--- scripts/create_gif.py
+++ scripts/create_gif.py
@@ -6,7 +6,8 @@ import imageio
 
 def create_gif(file_path, timepoints, duration=10, delete_img=True):
     images = [imageio.imread(f"{file_path}/step_{i:03d}.png") for i in timepoints]
-    imageio.mimsave(f"{file_path}/{file_path}_sim.gif", images, duration=duration)
+    gif_path = f"{file_path}/{file_path}_sim.gif"
+    imageio.mimsave(gif_path, images, duration=duration, loop=0)
 
     if delete_img:
         images = glob.glob(f"{file_path}/step_*.png")
