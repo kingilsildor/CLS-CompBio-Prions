8f08a1846372337e1794215fd0e2dc1e21745f44
kingilsildor <tychostam@gmail.com>
2025-05-22 09:53:10 +0200

    fixed the units

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-MAX_AGE = 200  # in timesteps
+# Age and death parameters
+MAX_AGE = 200  # in days
 MIN_AGE = 75
 GAMMA = 1e-3
 DELTA = 0.03454
@@ -8,20 +9,20 @@ HEALTH_NEURON = 1
 DEATH_NEURON = -1
 DEATH_MIN = -5
 
-SECRETED_VALUE = 1
-PRION_THRESHOLD = 3000.0
-NUMBER_OF_NEURONS = 200
+SECRETED_VALUE = 4  # μm a day for each μg
+PRION_THRESHOLD = 100  # μg per μg neuron
+NUMBER_OF_NEURONS = 500
 
-GRID_SIZE = 50
-GRID_SPACING = 1.0
+GRID_SIZE = 50  # just a spaceing
+GRID_SPACING = 0.044  # in mm
 
-k_A = 0.04
-k_B = 0.05
-k_c = 0.5
-D_A = 0.5
-D_B = 0.05
+k_A = 0.7  # μg/μg/day
+k_B = 0.5  # μg/μg/day
+k_c = 0.6  # μg/μg/day
+D_A = 0.7  # mm^2/day
+D_B = 0.1  # mm^2/day
 
-TIME = 500
+TIME = 300
 SECONDS = 25  # timesteps
 SAVE_INTERVAL = 10
 GIF_DURATION = 100
